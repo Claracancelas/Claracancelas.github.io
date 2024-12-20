243c602416bd1732d803347d21fcde3c48a293b7
Claracancelas <ccancelas2004@gmail.com>
2024-12-20 12:05:53 +0100

    Add files via upload

---
+++ guiaturisticaibiza/script.js
@@ -0,0 +1,226 @@
+$(window).scroll(function(){
+
+
+    
+    //Si el elemento es visible
+    if ($(".parallax-1").visible(true)) {
+        //Añade la clase “visible”
+        $(".parallax-1").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".parallax-1").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".ind1").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind1").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind1").removeClass("visible");
+        }
+
+    //Si el elemento es visible
+    if ($(".ind2").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind2").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind2").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".ind3").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind3").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind3").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".ind5").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind5").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind5").removeClass("visible");
+        }
+    
+     //Si el elemento es visible
+     if ($(".ind6").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind6").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind6").removeClass("visible");
+        }
+    
+      //Si el elemento es visible
+      if ($(".ind7").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind7").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind7").removeClass("visible");
+        }
+    
+      //Si el elemento es visible
+      if ($(".ind8").visible(true)) {
+        //Añade la clase “visible”
+        $(".ind8").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".ind8").removeClass("visible");
+        }
+        
+    //Si el elemento es visible
+    if ($(".marco-2").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco-2").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco-2").removeClass("visible");
+        }
+       
+     //Si el elemento es visible
+       if ($(".marco").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco").removeClass("visible");
+        }
+
+    //Si el elemento es visible
+    if ($(".marco-1").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco-1").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco-1").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".marco-3").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco-3").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco-3").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".marco-4").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco-4").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco-4").removeClass("visible");
+        }
+    
+        //Si el elemento es visible
+     if ($(".marco-5").visible(true)) {
+        //Añade la clase “visible”
+        $(".marco-5").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".marco-5").removeClass("visible");
+        }
+    
+        if ($(".marco-6").visible(true)) {
+            //Añade la clase “visible”
+            $(".marco-6").addClass("visible");
+            } else {
+            //Si no es visible quita:
+            $(".marco-6").removeClass("visible");
+            }
+
+            if ($(".marco-8").visible(true)) {
+                //Añade la clase “visible”
+                $(".marco-8").addClass("visible");
+                } else {
+                //Si no es visible quita:
+                $(".marco-8").removeClass("visible");
+                }
+    
+     //Si el elemento es visible
+     if ($(".frase-animada").visible(true)) {
+        //Añade la clase “visible”
+        $(".frase-animada").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".frase-animada").removeClass("visible");
+        }
+    
+     //Si el elemento es visible
+     if ($(".avion").visible(true)) {
+        //Añade la clase “visible”
+        $(".avion").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".avion").removeClass("visible");
+        }
+    
+    //Si el elemento es visible
+     if ($(".texto-discoteca").visible(true)) {
+        //Añade la clase “visible”
+        $(".texto-discoteca").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $("   .texto-discoteca").removeClass("visible");
+        }
+
+     //Si el elemento es visible
+     if ($(".contenedor-imagen").visible(true)) {
+        //Añade la clase “visible”
+        $(".contenedor-imagen").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".contenedor-imagen").removeClass("visible");
+        }
+
+        //Si el elemento es visible
+     if ($(".contenedor-recomendaciones1").visible(true)) {
+        //Añade la clase “visible”
+        $(".contenedor-recomendaciones1").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".contenedor-recomendaciones1").removeClass("visible");
+        }
+        
+          //Si el elemento es visible
+     if ($(".contenedor-recomendaciones2").visible(true)) {
+        //Añade la clase “visible”
+        $(".contenedor-recomendaciones2").addClass("visible");
+        } else {
+        //Si no es visible quita:
+        $(".contenedor-recomendaciones2").removeClass("visible");
+        }
+
+        if ($(".contenedor-recomendaciones3").visible(true)) {
+            //Añade la clase “visible”
+            $(".contenedor-recomendaciones3").addClass("visible");
+            } else {
+            //Si no es visible quita:
+            $(".contenedor-recomendaciones3").removeClass("visible");
+            }
+
+            if ($(".contenedor-recomendaciones4").visible(true)) {
+                //Añade la clase “visible”
+                $(".contenedor-recomendaciones4").addClass("visible");
+                } else {
+                //Si no es visible quita:
+                $(".contenedor-recomendaciones4").removeClass("visible");
+                }
+ 
+   
+  
+    
+    
+   
+
+        
+
+
+    });
